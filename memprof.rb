8ae74eaa6fe40eaa29cb4ea77b0e84fa4086011f
Aman Gupta <aman@tmm1.net>
2010-02-27 20:45:11 -0800

    dynamic namespace view

--- memprof.rb
+++ memprof.rb
@@ -23,18 +23,43 @@ class MemprofApp < Sinatra::Default
   end
 
   get '/classview' do
-    if params[:where]
-      klass = $dump.db.find_one(Yajl.load params[:where])
+    if where = params[:where]
+      klass = $dump.db.find_one(Yajl.load where)
       subclasses = $dump.subclasses_of(klass).sort_by{ |o| o['name'] || '' }
+    elsif root = params[:root]
+      subclasses = [$dump.db.find_one(Yajl.load root)]
     else
       subclasses = [$dump.root_object]
     end
 
+    subclasses.compact!
     subclasses.each do |o|
-      o['hasChildren'] = $dump.subclasses_of?(o)
+      o['hasSubclasses'] = $dump.subclasses_of?(o)
+      # o['numInstances'] = $dump.db.find(:type => 'object', :class => o['_id']).count
     end
 
     partial :_classview, :layout => (request.xhr? ? false : :ui), :list => subclasses
+  end
+
+  get '/namespace' do
+    if where = params[:where]
+      obj = $dump.db.find_one(Yajl.load where)
+    else
+      obj = $dump.root_object
+    end
+
+    constants = obj['ivars'].reject{ |k,v| k !~ /^[A-Z]/ }
+    names = constants.invert
+    classes = $dump.db.find(:type => {:$in => %w[iclass class module]}, :_id => {:$in => constants.values}).to_a.sort_by{ |o| names[o['_id']] || o['name'] }
+
+    classes.each do |o|
+      unless o['name'] == 'Object'
+        vars = o['ivars'].reject{ |k,v| k !~ /^[A-Z]/ }
+        o['hasChildren'] = !!$dump.db.find_one(:type => {:$in => %w[iclass class module]}, :_id => {:$in => vars.values})
+      end
+    end
+
+    partial :_namespace, :layout => (request.xhr? ? false : :ui), :list => classes, :names => names
   end
 
   helpers do
