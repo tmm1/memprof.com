60ef56222fb24acff28e2f4122bd948d63267408
Aman Gupta <aman@tmm1.net>
2010-03-20 22:01:54 -0700

    replay, zomg

--- public/app.js
+++ public/app.js
@@ -23,6 +23,12 @@ $.fn.setupTree = function() {
           url: url,
           success: function(data) {
             sublist.animate({height: '0'}, 'fast', function(){
+              var panel = $this.parents('div.panel:first');
+              var hash = panel.attr('url') || '';
+              panel.attr('url', hash + '//TOGGLE=' + url);
+              updateHash();
+              replayNext(panel);
+
               sublist.remove();
               var newlist = $(data);
               newlist.css('display','none').appendTo($this);
@@ -50,6 +56,9 @@ $.fn.setupPanel = function(){
       panel.addClass('additional');
 
     panel.find('pre.prettyprint').prettify();
+
+    updateHash();
+    replayNext(panel);
   });
 };
 
@@ -62,6 +71,37 @@ $.fn.prettify = function(){
     container.html(prettyPrintOne(container.html()));
   });
 };
+
+var replayNext = function(panel){
+  var link = REPLAY.shift();
+  if (link) {
+    setTimeout(function(){
+      if (link.match(/^TOGGLE=/)) {
+        var ul = panel.find("ul[url="+link.slice(7)+"]");
+        if (ul.length) {
+          ul.prevAll('div.hitarea:first').click();
+        }
+      } else {
+        var links = panel.find("a:contains("+link+")");
+        if (links.length > 0) {
+          var preferred = links.filter(function(){ return $(this).text().trim() == "element_name"; });
+          if (preferred.length > 0)
+            preferred.eq(0).click();
+          else
+            links.eq(0).click();
+        }
+      }
+    }, 300);
+  }
+};
+
+var updateHash = function(){
+  window.location.hash = '';
+  $('div.panel').each(function(){
+    window.location.hash += ($(this).attr('url') || '');
+    window.location.hash += ('//' + $(this).find('a.current').text().trim());
+  });
+}
 
 var updateBodyWidth = function(){
   var w = 0, num = 0;
@@ -103,10 +143,14 @@ var centerPanel = function(panel, to_top) {
   if (window.pageYOffset > bottom && !to_top && y == 0)
     to_top = true;
 
+  var after = function(){
+    scrollingTo = false;
+  }
+
   if (to_top || y > 0)
-    $.scrollTo({left:x, top:y}, 'fast', {queue:false, onAfter:function(){ scrollingTo = false }});
+    $.scrollTo({left:x, top:y}, 'fast', {queue:false, onAfter:after});
   else
-    $.scrollTo(x, 'fast', {axis:'x', onAfter:function(){ scrollingTo = false }});
+    $.scrollTo(x, 'fast', {axis:'x', onAfter:after});
 };
 
 var findClosestPanel = function(){
@@ -128,6 +172,67 @@ var findClosestPanel = function(){
 
   return showPanel;
 };
+
+$('ul.nav li a:not(.popout)').live('click', function(){
+  var link = $(this);
+  var nav = $(this).parents('ul.nav:first');
+  nav.find('a.selected').removeClass('selected');
+  $(this).addClass('selected');
+
+  var panel = $(this).parents('div.panel:first');
+  panel.nextAll().remove();
+  panel.find('> div.content').html('<center><img src="/demo/spinner.gif" style="margin: auto"></center>');
+
+  $.get(this.href, function(html){
+    var newPanel = $(html);
+    var hash = panel.attr('url') || '';
+
+    panel.replaceWith(newPanel);
+    newPanel.attr('url', hash + '//' + link.text().trim());
+    newPanel.setupPanel();
+
+    updateBodyWidth();
+    centerPanel(newPanel, true);
+  });
+
+  return false;
+});
+
+$('div.panel .content a').live('click', function(){
+  var link = $(this);
+  var curPanel = $(this).parents('div.panel:first');
+
+  curPanel.find('a.current').removeClass('current');
+  link.addClass('current');
+
+  var panel = $('<div class="panel additional"><center><img src="/demo/spinner.gif"></center></div>');
+  curPanel.nextAll().remove().end().after(panel);
+
+  $.get(this.href, function(html){
+    link.addClass('current');
+
+    var newPanel = $(html);
+    panel.replaceWith(newPanel);
+    newPanel.setupPanel();
+
+    updateBodyWidth();
+    centerPanel(newPanel, true);
+  });
+
+  return false;
+});
+
+$('div.panel ul.nav li.group select.group_key').live('change', function(){
+  var select = $(this);
+  var link = select.parents('a:first');
+  link.attr('href', link.attr('href').replace(/&key=.*$/,'') + "&key=" + select.val());
+  link.click();
+});
+
+$('div#menubar select.collection').live('change', function(){
+  var select = $(this);
+  window.location = '/db/' + select.val();
+});
 
 $(function(){
   $(window).keydown(function(e){
@@ -162,113 +267,13 @@ $(function(){
     }
   });
 
-  /*
-  var scrollTimeout = null;
-  $(window).scroll(function(){
-    if (scrollingTo)
-      return;
+  REPLAY = window.location.hash.split('//').slice(1);
 
-    if (scrollTimeout) {
-      clearTimeout(scrollTimeout);
-      scrollTimeout = 0;
-    }
+  var panel = $('div.panel')
+  panel.setupPanel();
 
-    scrollTimeout = setTimeout(function(){
-      scrollTimeout = 0;
-      if (scrollingTo)
-        return;
-
-      var showPanel = findClosestPanel();
-      if (showPanel)
-        centerPanel(showPanel);
-    }, 200);
-  });
-  */
-
-  $('div.panel').setupPanel();
-  var width = $('div.panel').outerWidth();
+  var width = panel.outerWidth();
   $('body').css('marginLeft', ($(window).width()/2 - width/2) + 'px');
-
-  /*
-  var input = $('#query input');
-  var updateSubnav = function(){
-    if (inputVal == input.val()) return;
-    inputVal = input.val();
-
-    input.siblings('.objects').html('<center><img src="/demo/spinner.gif" align="absmiddle" /></center>');
-
-    $.getJSON(
-      '/subnav',
-      { where: JSON.stringify(input.val()) },
-      function(obj){
-        input.siblings('.objects').text(obj.count + ' object' + (obj.count==1 ? '' : 's'));
-      }
-    );
-  };
-
-  input.blur(updateSubnav);
-  input.focus(function(){ input.css({padding: '5px 10px 5px 5px', textAlign: 'left'}) })
-       .blur( function(){ input.css({padding: '5px', textAlign: 'center'}) });
-
-  input.blur();
-  */
-
-  var form = $('form#search');
-
-  $('ul.nav li a:not(.popout)').live('click', function(){
-    var nav = $(this).parents('ul.nav:first');
-    nav.find('a.selected').removeClass('selected');
-    $(this).addClass('selected');
-
-    var panel = $(this).parents('div.panel:first');
-    panel.nextAll().remove();
-    panel.find('> div.content').html('<center><img src="/demo/spinner.gif" style="margin: auto"></center>');
-
-    $.get(this.href, function(html){
-      var newPanel = $(html);
-      panel.replaceWith(newPanel);
-      newPanel.setupPanel();
-      updateBodyWidth();
-      centerPanel(newPanel, true);
-    });
-
-    return false;
-  });
-
-  $('div.panel .content a').live('click', function(){
-    var link = $(this);
-    var curPanel = $(this).parents('div.panel:first');
-
-    curPanel.find('a.current').removeClass('current');
-    link.addClass('current');
-
-    var panel = $('<div class="panel additional"><center><img src="/demo/spinner.gif"></center></div>');
-    curPanel.nextAll().remove().end().after(panel);
-
-    $.get(this.href, function(html){
-      link.addClass('current');
-
-      var newPanel = $(html);
-      panel.replaceWith(newPanel);
-      newPanel.setupPanel();
-      updateBodyWidth();
-      centerPanel(newPanel, true);
-    });
-
-    return false;
-  });
-
-  $('div.panel ul.nav li.group select.group_key').live('change', function(){
-    var select = $(this);
-    var link = select.parents('a:first');
-    link.attr('href', link.attr('href').replace(/&key=.*$/,'') + "&key=" + select.val());
-    link.click();
-  });
-
-  $('div#menubar select.collection').live('change', function(){
-    var select = $(this);
-    window.location = '/db/' + select.val();
-  });
 
   $('pre.prettyprint').prettify();
 });
