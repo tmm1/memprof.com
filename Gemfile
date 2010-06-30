c13458014c786fc2b6c98dab41754feb919f8590
Aman Gupta <aman@tmm1.net>
2010-06-29 19:37:18 -0700

    working stats view

--- Gemfile
+++ Gemfile
@@ -1,15 +1,21 @@
 source :rubygems
 
-gem 'sinatra', '0.9.6'
+gem 'sinatra'
 gem 'thin'
 gem 'haml'
 gem 'sequel'
 gem 'rack'
 gem 'yajl-ruby'
-gem 'mongo', '0.19.1'
-gem 'mongo_ext', '0.19.1'
+#gem 'mongo', '0.19.1'
+#gem 'mongo_ext', '0.19.1'
+gem 'mongo'
+gem 'bson'
+gem 'bson_ext'
 gem 'ruby2ruby'
 gem 'ruby_parser'
 gem 'ParseTree'
 gem 'bcrypt-ruby'
 gem 'rake'
+
+
+gem 'rails', '3.0.0.beta4'
--- hotspots.rb
+++ hotspots.rb
@@ -7,6 +7,13 @@ require 'yajl'
 require 'bcrypt'
 require 'securerandom'
 
+require 'action_view/helpers/number_helper'
+require 'i18n'
+require 'active_support/core_ext/array/extract_options'
+require 'active_support/core_ext/hash/keys'
+require 'active_support/core_ext/hash/reverse_merge'
+require 'active_support/core_ext/string/output_safety'
+
 require 'mongo'
 CONN = Mongo::Connection.new
 DB = CONN.db('hotspots')
@@ -21,27 +28,11 @@ class Hotspots < Sinatra::Base
   end
 
   get '/' do
-    haml :foobar
+    @result = DB.collection('github_stats')
+    haml :stats
   end
 
-  get '/stats' do
-    # result = @db.map_reduce(
-    #   "function(){
-    #     var t = this.tracers,
-    #         result = {
-    #           time: [this.time],
-    #           objects_created: [],
-    #           gc_time: [],
-    #           mysql_time: [],
-    #           memcache_get_responses_notfound: []
-    #         };
-    #     
-    #   }",
-    #   "function(){
-    #     
-    #   }"
-    # )
-
+  get '/generate' do
     result = @db.map_reduce(
       "function(){
         var t = this.tracers;
@@ -83,66 +74,91 @@ class Hotspots < Sinatra::Base
         return result;
       }",
       :sort => ['value.time.count', :desc],
-      :finalize => 'function(key, val){
+      :finalize => %[function(key, val){
+        var stats = function(data) {
+            data.sort(function(a,b){ return a-b; });
+
+            var s = {};
+            s.count = data.length;
+            if (!s.count)
+              return s;
+
+            s.min = data[0];
+            s.max = data[data.length - 1];
+
+            var calc_median = function(pos, len) {
+              if ((len % 2) != 0)
+                  return data[pos];
+              else
+                  return (data[pos - 1] + data[pos]) / 2;
+            }
+
+            var middle = Math.floor(data.length / 2);
+
+            s.median = calc_median(middle,                          data.length);
+            s.q1     = calc_median(Math.floor(middle / 2),          middle);
+            s.q2     = calc_median(Math.floor(data.length * 0.75),  data.length - middle);
+
+            var i=0, sum=0
+            for(; i < list.length; sum += list[i++]);
+
+            s.avg = sum / s.count;
+            return s;
+        }
+
         var result = {};
 
         for (var f in val) {
           var list = val[f];
-          list.sort(function(a,b){ return a-b; });
+          result[f] = stats(list);
 
-          for(var i=0,sum=0; i<list.length; sum+=list[i++]);
-
-          result[f] = {
-            count: list.length,
-            min: list[0],
-            max: list[list.length-1]
-          };
-
-          if (result[f].count)
-            result[f].avg = sum/result[f].count;
+          // list.sort(function(a,b){ return a-b; });
+          // 
+          // for(var i=0,sum=0; i<list.length; sum+=list[i++]);
+          // 
+          // result[f] = {
+          //   count: list.length,
+          //   min: list[0],
+          //   max: list[list.length-1]
+          // };
+          // 
+          // if (result[f].count)
+          //   result[f].avg = sum/result[f].count;
         }
 
         return result;
-      }'
+      }],
+      :out => 'github_stats'
     )
 
-    p result
-    p result.count
-    @result = result
-
-    haml :stats
+    "Done w/ #{result.count} entries"
   end
 
   helpers do
     include Rack::Utils
     alias_method :h, :escape_html
+    include ActionView::Helpers::NumberHelper
+    def number_with_delimiter(num,opts={})
+      super(num, opts.merge(:delimiter => ',', :separator => '.'))
+    end
+
+    def data_format(data, format)
+      if format == '%d'
+        number_with_delimiter(data.to_i)
+      else
+        format % data
+      end
+    end
   end
 
   set :server, 'thin'
   set :port, 7006
   set :public, File.expand_path('../public', __FILE__)
-  enable :static, :logging, :inline_templates
+  enable :static, :logging
+  # enable :inline_templates
   use Rack::Session::Cookie, :key => 'memprof_hotspots', :secret => 'noisses_forpmem', :expire_after => 2592000
 end
 
 if __FILE__ == $0
   Hotspots.run!
 end
-
-__END__
-
-@@ foobar
-hello world!
-
-@@ stats
-%table
-  %thead
-    %tr
-      %th action
-      %th 
-  %tbody
-    - @result.find.limit(10).each do |row|
-      %tr
-        %td
-          %p
-            %pre= row.inspect
